33e03dd6341b68427949f0a7913bb815a51033f0
Elliot Cheung <elliotccheung@gmail.com>
2017-02-08 22:33:51 +0000

    Gave the ticketing page some proper sample text

--- client/components/ticketing.js
+++ client/components/ticketing.js
@@ -8,7 +8,9 @@ export default class Ticketing extends React.Component {
   render() {
     return (
       <div id="ticketingPage">
-        Lorem ipsum blah blah blah
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacus dui, bibendum id efficitur id, pretium sed nulla. Aenean congue, lorem at elementum tristique, quam massa ornare tortor, vitae dignissim lacus tortor a est. Aenean elit est, convallis vitae purus ut, auctor rhoncus mi. Donec viverra urna elit. Duis molestie posuere lectus. Vivamus tempor diam id ipsum maximus, lobortis volutpat eros hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi cursus, justo at ullamcorper posuere, neque eros vestibulum leo, mollis mattis enim neque eget tortor. Curabitur vestibulum nibh et ullamcorper egestas. Phasellus vestibulum ac metus id luctus. Praesent in porttitor massa.
+
+Vivamus vel sollicitudin urna. Curabitur tempor iaculis erat, ut dignissim urna pretium cursus. In velit enim, cursus sit amet purus in, suscipit ornare odio. Ut finibus consectetur mauris quis dapibus. Mauris lacinia tortor quis nunc imperdiet tristique. In tellus sem, aliquet vitae est quis, convallis mattis velit. Proin feugiat sapien vitae libero auctor, a euismod neque efficitur. Mauris gravida tellus et quam finibus, at rhoncus dolor efficitur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. Donec eu hendrerit massa. Quisque eu orci at est ullamcorper interdum non sed est. Suspendisse sollicitudin neque vel sapien rutrum, sed viverra neque fringilla. Vivamus quis lectus odio. Quisque ultricies fermentum eleifend. Sed id semper lorem.
       </div>
       )
   }
