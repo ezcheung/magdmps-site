8b27f252f6ba2fac7d00562d8edeaee2b639eaaa
Elliot Cheung <elliotccheung@gmail.com>
2017-02-08 15:46:40 +0000

    Set up spiel about play

---
+++ client/components/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class About extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div id="spiel">
+        <p>
+        Join us aboard the SS American for a night of love, scandal and classic showtunes!
+        </p>
+        <p>
+        The multi award-winning show with music by Cole Porter and book by PG Woodhouse is coming to the Magadelene Cripps Auditorium! Join former evangelist turned nightclub singer Reno Sweeney, debutante Hope Harcourt, stowaway Billy Crocker and second-rate gangster Moonface Martin (Public Enemy #13) on their transatlantic voyage from New York to London to find out what happens when convention goes out the porthole!
+        </p>
+      </div>
+      )
+  }
+}
--- client/components/app.js
+++ client/components/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Banner from './banner';
+import About from './about';
 
 export default class App extends React.Component {
   
@@ -15,6 +16,7 @@ export default class App extends React.Component {
     return (
       <div id="app">
         <Banner/>
+        <About/>
       </div>
       )
   }
