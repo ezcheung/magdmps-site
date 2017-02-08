158653c034de2369ffbbced3d46e85a3a7a389c1
Elliot Cheung <elliotccheung@gmail.com>
2017-02-08 21:50:44 +0000

    Set up castList file

---
+++ client/components/castList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class CastList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="castList">
+      </div>
+    )
+  }
+}
