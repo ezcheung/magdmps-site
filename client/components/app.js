98f8a049ac973490377286e386dbead5370908e5
Elliot Cheung <elliotccheung@gmail.com>
2017-02-08 10:37:13 +0000

    Set up banner

--- client/components/app.js
+++ client/components/app.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
+import Banner from './banner';
+
 export default class App extends React.Component {
   
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Banner/>
+      </div>
+      )
+  }
 }
---
+++ client/components/banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Banner extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className="banner">
+        <h1>Anything Goes</h1>
+      </div>
+    )
+  }
+}
