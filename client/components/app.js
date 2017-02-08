badfb35e5f276de1acc7e432f09ac3b9a9070961
Elliot Cheung <elliotccheung@gmail.com>
2017-02-08 22:32:34 +0000

    Cast List page is now functional

--- client/components/app.js
+++ client/components/app.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Banner from './banner';
 import About from './about';
 import Ticketing from './ticketing';
+import CastList from './castList';
 
 export default class App extends React.Component {
   
@@ -23,6 +24,9 @@ export default class App extends React.Component {
     }
     if (this.state.currentPage === 'tickets') {
       return <Ticketing/>;
+    }
+    if (this.state.currentPage === 'cast') {
+      return <CastList/>;
     }
   }
 
--- client/components/castList.js
+++ client/components/castList.js
@@ -3,11 +3,73 @@ import React from 'react';
 export default class CastList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.cast = `Billy Crocker - Adam Long
+Moonface Martin - Alex Franklin
+Reno Sweeney - Martha Cook
+Hope Harcourt - Ruth Warner
+Sir Evelyn Oakleigh - Luke Dell
+Mrs. Harcourt - Becky Shepherdson
+Elisha Whitney - Joe Pieri
+Captain - Freddie Raymond
+Purser - Sam Ashbridge
+Steward - Anna Culkin
+Purity - Maddy Cummins
+Bishop Dobson - Ollie Hayes
+Reporter - Kasia Ruszkowski
+Cameraman - Katie Wickham
+Gambler - Anna Smith
+Sailor - Arthur Lee, Theo Heymann
+Bonnie - Charlotte Guerry
+Gambler - Michelle Spielberg
+Chastity - Katie McCoshan
+Charity - Julia Hubo
+Virtue - Ellen Harris`.split('\n');
+
+    this.band = 
+    `Keys 1 - Joanna Cheng
+Keys 2 - Michael Cullen
+Bass - Sam Brown
+Bass Dep - Hazel Ford
+Guitar - Max Goodall
+Reed I - Will Reis
+Reed II - Sam Kirby
+Trumpet - Sally Jenkinson
+Trombone - Ewan Day-Collins, Max Wilkinson
+Percussion - Kevin Heffernan`.split('\n');
+
+    this.team = `Director - Lily Burge
+Musical Director - Joanna Cheng
+Executive Producer - Rosie Causer
+Producer - Gabriel Humphreys
+Choreographer - Anna Smith
+Set Designer - Samantha Ruston
+Assistant Musical Director - Michael Cullen
+Costume Designer - Beth Forsyth
+Lighting Designer - Daniel Lawson
+Publicity Designer - Eliza Le Roy-Lewis`.split('\n');
+
+  }
+
+  makeList(arr, title) {
+    return (
+      <div className="list">
+        <h3 className="listTitle">{title}</h3>
+        {arr.map((p, i) => 
+          <div className='listMember' key={i}>
+            {p}
+          </div>
+        )}
+      </div>
+      )
   }
 
   render() {
     return (
-      <div id="castList">
+      <div id="peopleLists">
+        {this.makeList(this.cast, "Cast")}
+        {this.makeList(this.band, "Band")}
+        {this.makeList(this.team, "Production Team")}
       </div>
     )
   }
