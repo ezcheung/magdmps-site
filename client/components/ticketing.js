5faba1db87da41b6fc093a4b0bbb9828cd9c7934
Elliot Cheung <elliotccheung@gmail.com>
2017-02-12 16:38:33 +0000

    Moved paypal button HTML to separate function

--- client/components/ticketing.js
+++ client/components/ticketing.js
@@ -5,30 +5,36 @@ export default class Ticketing extends React.Component {
     super(props)
   }
 
-  render() {
+  payForm() {
     return (
-      <div id="ticketingPage">
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
         <input type="hidden" name="cmd" value="_s-xclick"/>
         <input type="hidden" name="hosted_button_id" value="3EDV7YQPLYB4S"/>
         <table>
         <tr><td><input type="hidden" name="on0" value="Showtimes"/>Showtimes</td></tr><tr><td><select name="os0">
-          <option value="Tuesday (21/2) - Concession">Tuesday (21/2) - Concession £5.00 GBP</option>
-          <option value="Tuesday (21/2)">Tuesday (21/2) £8.00 GBP</option>
-          <option value="Wednesday (22/2) - Concession">Wednesday (22/2) - Concession £5.00 GBP</option>
-          <option value="Wednesday (22/2)">Wednesday (22/2) £8.00 GBP</option>
-          <option value="Thursday (23/2) - Concession">Thursday (23/2) - Concession £5.00 GBP</option>
-          <option value="Thursday (23/2)">Thursday (23/2) £8.00 GBP</option>
-          <option value="Friday (24/2) - Concession">Friday (24/2) - Concession £5.00 GBP</option>
-          <option value="Friday (24/2)">Friday (24/2) £8.00 GBP</option>
-          <option value="Saturday (25/2) - Concession">Saturday (25/2) - Concession £5.00 GBP</option>
-          <option value="Saturday (25/2)">Saturday (25/2) £8.00 GBP</option>
+          <option value="Tuesday (21/2) - Concession">Tuesday (21/2) - Concession</option>
+          <option value="Tuesday (21/2)">Tuesday (21/2)</option>
+          <option value="Wednesday (22/2) - Concession">Wednesday (22/2) - Concession</option>
+          <option value="Wednesday (22/2)">Wednesday (22/2)</option>
+          <option value="Thursday (23/2) - Concession">Thursday (23/2) - Concession</option>
+          <option value="Thursday (23/2)">Thursday (23/2)</option>
+          <option value="Friday (24/2) - Concession">Friday (24/2) - Concession</option>
+          <option value="Friday (24/2)">Friday (24/2)</option>
+          <option value="Saturday (25/2) - Concession">Saturday (25/2) - Concession</option>
+          <option value="Saturday (25/2)">Saturday (25/2)</option>
         </select> </td></tr>
         </table>
         <input type="hidden" name="currency_code" value="GBP"/>
-        <input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!"/>
+        <input className="submitBtn" type="submit" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!"/>
         <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1"/>
-        </form>
+      </form>
+    )
+  }
+//src="https://www.paypalobjects.com/en_GB/i/btn/btn_buynow_LG.gif"
+  render() {
+    return (
+      <div id="ticketingPage">
+        {this.payForm()}
       </div>
       )
   }
